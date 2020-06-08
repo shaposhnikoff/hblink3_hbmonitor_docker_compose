c7186eb9cc3463c08aa80106b2b51889f95047c4
Maksym Shaposhnikov <shaposhnikoff@gmail.com>
2020-06-08 23:24:00 +0300

    2020-06-08-232359 Don't push this commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # hblink3_hbmonitor_docker_compose
 
 1. git clone --recurse-submodules git@github.com:shaposhnikoff/hblink3_hbmonitor_docker_compose.git
+
+2.1 create folder
+Add your configuration
+
 2. ./docker-compose.restart
 
---
+++ commit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+find . -mindepth 2 -name ".git" | xargs rm -rf
+
+# git push ironman repo to github
+
+dated=`date +%Y-%m-%d-%H%M%S`
+
+git add .
+echo run git push on ${dated}
+
+
+for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
+
+# git add -u .
+#
+# git commit -m 'some fix on '${dated} # nice ;-)
+
+git commit -m "$dated $(curl -s http://whatthecommit.com/index.txt)"
+git push -u origin master
